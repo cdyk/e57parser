1af3608ad261e2af3e028b46ed4f96a1e863b0d8
Christopher Dyken <cdyk@users.noreply.github.com>
2023-03-22 23:25:44 +0100

    Add arena code

--- src/Common.cpp
+++ src/Common.cpp
@@ -3,6 +3,8 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <cassert>
+#include <algorithm>
 
 #include "Common.h"
 
@@ -36,4 +38,61 @@ void* xrealloc(void* ptr, size_t size)
 
   fprintf(stderr, "Failed to allocate memory.");
   exit(-1);
+}
+
+void* Arena::alloc(size_t bytes)
+{
+  const size_t pageSize = 1024 * 1024;
+
+  if (bytes == 0) return nullptr;
+
+  size_t padded = (bytes + 7) & ~7;
+
+  if (size < fill + padded) {
+    fill = sizeof(uint8_t*);
+    size = std::max(pageSize, fill + padded);
+
+    auto* page = (uint8_t*)xmalloc(size);
+    *(uint8_t**)page = nullptr;
+
+    if (first == nullptr) {
+      first = page;
+      curr = page;
+    }
+    else {
+      *(uint8_t**)curr = page; // update next
+      curr = page;
+    }
+  }
+
+  assert(first != nullptr);
+  assert(curr != nullptr);
+  assert(*(uint8_t**)curr == nullptr);
+  assert(fill + padded <= size);
+
+  uint8_t* rv = curr + fill;
+  fill += padded;
+  return rv;
+}
+
+void* Arena::dup(const void* src, size_t bytes)
+{
+  auto* dst = alloc(bytes);
+  std::memcpy(dst, src, bytes);
+  return dst;
+}
+
+
+void Arena::clear()
+{
+  auto* c = first;
+  while (c != nullptr) {
+    auto* n = *(uint8_t**)c;
+    free(c);
+    c = n;
+  }
+  first = nullptr;
+  curr = nullptr;
+  fill = 0;
+  size = 0;
 }
--- src/Common.h
+++ src/Common.h
@@ -45,6 +45,25 @@ struct Buffer : public BufferBase
   void accommodate(size_t count) { _accommodate(sizeof(T), count); }
 };
 
+struct Arena
+{
+  Arena() = default;
+  Arena(const Arena&) = delete;
+  Arena& operator=(const Arena&) = delete;
+  ~Arena() { clear(); }
+
+  uint8_t* first = nullptr;
+  uint8_t* curr = nullptr;
+  size_t fill = 0;
+  size_t size = 0;
+
+  void* alloc(size_t bytes);
+  void* dup(const void* src, size_t bytes);
+  void clear();
+
+  template<typename T> T* alloc() { return new(alloc(sizeof(T))) T(); }
+};
+
 
 bool e57Parser(Logger logger, const char* path, const char* ptr, size_t size);
 bool parseE57Xml(Logger logger, const char* xmlBytes, size_t xmlLength);
